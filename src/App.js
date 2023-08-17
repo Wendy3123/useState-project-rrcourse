cbe032f850b96d145d544df7cb2ab090b7020dda
Wendy Wang <wendywangg928@gmail.com>
2023-08-17 18:47:14 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ export default function App() {
 function Counter() {
   const [count, setCount] = useState(0);
   const [step, setStep] = useState(1);
-  const myDate = new Date("August 17 2023"); //dont worry much of the date part just understand the useState()
+  const myDate = new Date("Aug 17 2023"); //dont worry much of the date part just understand the useState()
   myDate.setDate(myDate.getDate() + count);
   return (
     <>
@@ -30,7 +30,19 @@ function Counter() {
           <button onClick={() => setCount(count + step)}>+</button>
         </div>
       </div>
-      <p>{myDate.toDateString()}</p>
+      <p>
+        <span>
+          {
+            count === 0
+              ? "Today is " //starting from the myDate var u give count 0 = myDate
+              : count > 0 //else if count is greater than 0 days
+              ? `${count} day(s) from today is ` //print the number of the var count along with the future date
+              : `${Math.abs(count)} days ago was ` //anything else meaning below count=0 or negative numbers print past dates
+            //Math.abs returns absolute value so instead of print -2 days ago it will print 2 days ago
+          }
+        </span>
+        <span>{myDate.toDateString()}</span>
+      </p>
     </>
   );
 }
